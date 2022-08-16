abff6358fd33d0a10391cfedc644b45d9ff93c52
hobbitKrd <za.css@yandex.ru>
2022-08-16 19:35:50 +0300

    add Lesson3

---
+++ HomeWorkSeminar/Lesson3/HomeWorkLesson3.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace HomeWorkSeminar.Lesson3
+{
+
+    internal class HomeWorkLesson3
+    {
+        public struct Point3d
+        {
+            public float X { get; set; }
+            public float Y { get; set; }
+            public float Z { get; set; }
+            public Point3d(float x, float y, float z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+
+        }
+        public bool isPallindromI(int numLikeInt)
+        {
+            int tempNum = numLikeInt;
+            int workNum = 0;
+            while (numLikeInt > 0)
+            {
+                int dig = numLikeInt % 10;
+                workNum = workNum * 10 + dig;
+                numLikeInt = numLikeInt / 10;
+            }
+            return workNum == tempNum ? true : false;
+        }
+        public bool isPallindromS(int numToString)
+        {
+            var tempNum = numToString.ToString();
+            return tempNum.SequenceEqual(tempNum.Reverse());
+        }
+        public float TwoPointDistance(Point3d pointOne, Point3d pointTwo)
+        {
+            return MathF.Sqrt(
+                        MathF.Pow((pointTwo.X - pointOne.X), 2) +
+                        MathF.Pow((pointTwo.Y - pointOne.Y), 2) +
+                        MathF.Pow((pointTwo.Z - pointOne.Z), 2));
+            //D=sqrt((X2-X1)^2 + (Y2-Y1)^2 + (Z2 - Z1)^2)
+        }
+        public string CoubTable(int num)
+        {
+            var temp = new List<int>();
+            for (int i = 1; i <= num; i++)
+            {
+                temp.Add(i*i*i);
+            }
+            return string.Join(",", temp);
+        }
+    }
+}
--- HomeWorkSeminar/Program.cs
+++ HomeWorkSeminar/Program.cs
@@ -1,7 +1,23 @@
 ﻿
 using HomeWorkSeminar.Lesson2;
+using HomeWorkSeminar.Lesson3;
 
+#region HWL2
 var hwl2 = new HomeWorkLesson2();
 var allFunc = new string[3] { hwl2.SecondNum(367), hwl2.TNum(3232), hwl2.isWeekend(6) };
-Console.WriteLine(string.Join("/",allFunc));
+Console.WriteLine(string.Join("\n", allFunc));
+#endregion
+#region HWL3
+
+var hwl3 = new HomeWorkLesson3();
+var allFuncL3 = new string[]
+{
+    hwl3.isPallindromS(16461).ToString(),
+    hwl3.isPallindromI(16461).ToString(),
+    hwl3.TwoPointDistance(new HomeWorkLesson3.Point3d(3,6,8),new HomeWorkLesson3.Point3d(2,4,-3)).ToString(),
+    hwl3.CoubTable(5)
+};
+Console.WriteLine(string.Join("\n", allFuncL3));
+#endregion
 Console.ReadLine();
+
