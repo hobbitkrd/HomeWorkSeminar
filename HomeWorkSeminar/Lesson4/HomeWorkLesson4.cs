da58e231f7a8f4364d63aefc8b8c662305f09e96
hobbitKrd <za.css@yandex.ru>
2022-08-16 20:30:01 +0300

    add Lesson 4

---
+++ HomeWorkSeminar/Lesson4/HomeWorkLesson4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkSeminar.Lesson4
+{
+    internal class HomeWorkLesson4
+    {
+        public int MyPow(int A, int B)
+        {
+            var newA = A * A;
+            for (int i = 1; i < B - 1; i++) newA *= A;
+            return newA;
+        }
+        public int NumSum(int num)
+        {
+            var charnun = num.ToString()
+                            .ToCharArray();
+            var result = 0;
+            foreach (int n in charnun) result += n;
+            return result;
+        }
+        public int[] GenerateRandArray(int maxValue, int minValue = 0, int leght= 8)
+        { 
+            var rand = new Random();
+            var randArray = new int[leght];
+            for (int el=0;el< leght;el++) randArray[el] = rand.Next(minValue,maxValue);
+            return randArray;
+        }
+
+        public int BinaryToInt(int binary)
+        {
+            return Convert.ToInt32(binary.ToString(),2);
+        }
+        public string UnivConvert(float num)
+        {
+            var splited = num.ToString().Split(",");
+            return Convert.ToString(int.Parse(splited[0]),int.Parse(splited[1]));
+
+        }
+    }
+}
--- HomeWorkSeminar/Program.cs
+++ HomeWorkSeminar/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using HomeWorkSeminar.Lesson2;
 using HomeWorkSeminar.Lesson3;
+using HomeWorkSeminar.Lesson4;
 
 #region HWL2
 var hwl2 = new HomeWorkLesson2();
@@ -13,11 +14,25 @@ var hwl3 = new HomeWorkLesson3();
 var allFuncL3 = new string[]
 {
     hwl3.isPallindromS(16461).ToString(),
-    hwl3.isPallindromI(16461).ToString(),
+    hwl3.isPallindromI(16416).ToString(),
     hwl3.TwoPointDistance(new HomeWorkLesson3.Point3d(3,6,8),new HomeWorkLesson3.Point3d(2,4,-3)).ToString(),
     hwl3.CoubTable(5)
 };
 Console.WriteLine(string.Join("\n", allFuncL3));
 #endregion
+#region HWL4
+var hwl4 = new HomeWorkLesson4();
+var allFuncL4 = new string[]
+{
+    hwl4.MyPow(2, 4).ToString(),
+    hwl4.NumSum(9012).ToString(),
+    string.Join(" ", hwl4.GenerateRandArray(100)),
+    string.Join(" ", hwl4.GenerateRandArray(2, 0, 10)),
+    hwl4.UnivConvert(10.8f),
+    hwl4.BinaryToInt(1010).ToString(),
+};
+Console.WriteLine(string.Join("\n", allFuncL4));
+#endregion
+
 Console.ReadLine();
 
