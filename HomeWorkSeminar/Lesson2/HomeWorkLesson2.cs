255fe11d4a2a89ed913582e6e24db3cdd39655e6
hobbitKrd <za.css@yandex.ru>
2022-08-16 18:55:55 +0300

    Add project files.

---
+++ HomeWorkSeminar/Lesson2/HomeWorkLesson2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkSeminar.Lesson2
+{
+    internal class HomeWorkLesson2
+    {
+        public string SecondNum(int num)
+        {
+            if (num > 1) return $"{num.ToString()[2]} вторая цифра";
+            else return "Эй!Тут всего 1 цифра";
+        }
+        public string TNum(int num)
+        {
+            if (num > 2) return $"{num.ToString()[3]} третья цифра";
+            else return "Эй!Тут не 3 цифры";
+        }
+        public string isWeekend(int num)
+        {
+            if (num == 6 || num ==7) 
+            {
+                return "Можно пить пиво!";
+            }
+            else return "Эй,Работай!";
+        }
+
+    }
+}
---
+++ HomeWorkSeminar/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using HomeWorkSeminar.Lesson2;
+
+var hwl2 = new HomeWorkLesson2();
+var allFunc = new string[3] { hwl2.SecondNum(367), hwl2.TNum(3232), hwl2.isWeekend(6) };
+Console.WriteLine(string.Join("/",allFunc));
+Console.ReadLine();
