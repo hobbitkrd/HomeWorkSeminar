3d0b2884ab66fa06c1296275b743df977f1a2957
hobbitKrd <za.css@yandex.ru>
2022-08-24 18:34:49 +0300

    add HomeWorkLesson5

---
+++ HomeWorkSeminar/Lesson5/HomeWorkLesson5.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkSeminar.Lesson5;
+
+internal class HomeWorkLesson5
+{
+    public HomeWorkLesson5()
+    {
+        GenRandArray();
+    }
+    private int[] randArray { get; set; }
+    public int evenNumCount(int[] yourArray)
+    {
+        int count = 0;
+        foreach (var el in yourArray)
+        {
+            count += el % 2 == 0 ? 1 : 0;
+        }
+        return count;
+    }
+    public int evenNumCount()
+    {
+        int count = 0;
+        foreach (var el in randArray)
+        {
+            count += el % 2 == 0 ? 1 : 0;
+        }
+        return count;
+
+    }
+    public int evenPosition()
+    {
+        int evenPosSum = 0;
+        for (int i = 0; i < randArray.Length; i++)
+        {
+            if (i != 0 && i % 2 == 0) evenPosSum += randArray[i];
+        }
+        return evenPosSum;
+    }
+    public int MinMax()
+    {
+        randArray = randArray
+            .OrderBy(x => x)
+            .ToArray();
+        return randArray.Last() - randArray[0];
+    }
+
+
+    private void GenRandArray()
+    {
+        var rand = new Random();
+        randArray = new int[20];
+        for (int i = 0; i < randArray.Length; i++)
+        {
+            randArray[i] = rand.Next(100, 999);
+        }
+
+    }
+}
--- HomeWorkSeminar/Program.cs
+++ HomeWorkSeminar/Program.cs
@@ -1,38 +1,8 @@
-﻿
-using HomeWorkSeminar.Lesson2;
-using HomeWorkSeminar.Lesson3;
-using HomeWorkSeminar.Lesson4;
+﻿using HomeWorkSeminar.Lesson5;
 
-#region HWL2
-var hwl2 = new HomeWorkLesson2();
-var allFunc = new string[3] { hwl2.SecondNum(367), hwl2.TNum(3232), hwl2.isWeekend(6) };
-Console.WriteLine(string.Join("\n", allFunc));
-#endregion
-#region HWL3
-
-var hwl3 = new HomeWorkLesson3();
-var allFuncL3 = new string[]
-{
-    hwl3.isPallindromS(16461).ToString(),
-    hwl3.isPallindromI(16416).ToString(),
-    hwl3.TwoPointDistance(new HomeWorkLesson3.Point3d(3,6,8),new HomeWorkLesson3.Point3d(2,4,-3)).ToString(),
-    hwl3.CoubTable(5)
-};
-Console.WriteLine(string.Join("\n", allFuncL3));
-#endregion
-#region HWL4
-var hwl4 = new HomeWorkLesson4();
-var allFuncL4 = new string[]
-{
-    hwl4.MyPow(2, 4).ToString(),
-    hwl4.NumSum(9012).ToString(),
-    string.Join(" ", hwl4.GenerateRandArray(100)),
-    string.Join(" ", hwl4.GenerateRandArray(2, 0, 10)),
-    hwl4.UnivConvert(10.8f),
-    hwl4.BinaryToInt(1010).ToString(),
-};
-Console.WriteLine(string.Join("\n", allFuncL4));
-#endregion
-
+var hw5 = new HomeWorkLesson5();
+var randEven = hw5.evenNumCount();
+var even = hw5.evenNumCount(new int[] { 345, 897, 568, 234 });
+var evenSum = hw5.evenPosition();
+var minmax = hw5.MinMax();
 Console.ReadLine();
-
