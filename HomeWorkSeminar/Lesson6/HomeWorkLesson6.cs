b28cefab1db6c45ff3c0156f2d829e753ae336e1
hobbitKrd <za.css@yandex.ru>
2022-08-24 19:15:01 +0300

    add HomeWorkLesson6

---
+++ HomeWorkSeminar/Lesson6/HomeWorkLesson6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkSeminar.Lesson6
+{
+    internal class HomeWorkLesson6
+    {
+        public int OverZero(string numArray)
+        {
+            int count = 0;
+            var workArray = numArray.Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            foreach (var item in workArray) if (item > 0) count++;
+            return count;
+
+        }
+        public float[] IntersectionPoint(Point p1, Point p2)
+        {
+            float iPoint1 = -1*((float)(p2.X - p2.Y)/(float)(p1.X-p1.Y));
+            float iPoint2 = p1.Y * iPoint1 + p2.Y;
+            return new float[] { iPoint1, iPoint2 };
+        }
+    }
+}
--- HomeWorkSeminar/Program.cs
+++ HomeWorkSeminar/Program.cs
@@ -1,8 +1,20 @@
 ﻿using HomeWorkSeminar.Lesson5;
+using HomeWorkSeminar.Lesson6;
+using System.Drawing;
 
 var hw5 = new HomeWorkLesson5();
+var hw6 = new HomeWorkLesson6();
+
 var randEven = hw5.evenNumCount();
 var even = hw5.evenNumCount(new int[] { 345, 897, 568, 234 });
 var evenSum = hw5.evenPosition();
 var minmax = hw5.MinMax();
+
+Console.WriteLine("Введите любое количество чисел через пробел:");
+var numArray = Console.ReadLine();
+Console.WriteLine($"Кол-во чисел больше нуля: {hw6.OverZero(numArray)}");
+
+var iPoint = hw6.IntersectionPoint(new Point(6, 5), new Point(4, 6));
+Console.WriteLine(string.Join(" ",iPoint));
+
 Console.ReadLine();
